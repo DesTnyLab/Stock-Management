f1d21fd84918399022f0a5786edb7e20cf7b2bd4
destny69 <mzldestiny69@gmail.com>
2024-11-27 12:49:05 +0545

    chor: update

--- stock/views.py
+++ stock/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "stock/index.html")
+    return render(request, "stock/index.html", context={
+        'name': 'manjil'
+    })
