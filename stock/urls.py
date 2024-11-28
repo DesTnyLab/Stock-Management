b04da4df0bfdfa236a83b3d7ec69f3070a8a0c07
E Bsnt <the.bsnt.official@gmail.com>
2024-11-28 21:02:24 +0545

    chore: updating templates

--- stock/urls.py
+++ stock/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import manage_inventory
+from .views import manage_inventory, hello_world
+
 
 urlpatterns = [
-    path('', manage_inventory, name='manage_inventory'),
+    path("", manage_inventory, name="manage_inventory"),
+    path("hello", hello_world, name="hello_world"),
 ]
