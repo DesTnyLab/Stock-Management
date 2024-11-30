8872dce58ba0b0782d86329e068b97019ac0f5d6
destny69 <mzldestiny69@gmail.com>
2024-11-30 23:42:26 +0545

    chore: updating product details

--- stock/urls.py
+++ stock/urls.py
@@ -5,6 +5,8 @@ from .views import *
 urlpatterns = [
     path('', manage_inventory, name="manage_inventory"),
     path('<int:id>/', view_product_details, name= "product_history" ),
+    # path('product-report/<int:id>/', sales_and_purchase_report, name="product_report"),
+
     path('search-stock/', product_stock_search_ajax, name='product_stock_search_ajax'),  # AJAX search view for stock
 ]
 
